887393c9c93361dad35b06d4cb0b91a2e5ab5f63
ylahssini <ylahssini@gmail.com>
2020-10-25 01:10:15 +0100

    add env vars

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_search/src/home/home.dart';
 
-void main() {
+Future main() async {
+  await DotEnv().load('.env');
   runApp(MovieSearch());
 }
 
